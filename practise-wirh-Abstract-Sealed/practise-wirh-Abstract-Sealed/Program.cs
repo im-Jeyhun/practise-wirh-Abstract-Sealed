90421bb173ec973284a7e45e1d362374bbdf7034
Ceyhun <contact.ceyhun@gmail.com>
2022-07-19 21:22:25 +0400

    initial structure

---
+++ practise-wirh-Abstract-Sealed/practise-wirh-Abstract-Sealed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practise_wirh_Abstract_Sealed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
